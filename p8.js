945460bf6ee6737a291a8ba72f57e84f23dd14dd
Ashley-Frazier <afrazier316@gmail.com>
2021-11-15 15:56:14 -0500

    Reserve button

--- p8.js
+++ p8.js
@@ -1,3 +1,10 @@
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+var reserve = document.querySelectorAll('.reverse-button');
+
+reserve.forEach(item => {
+	item.addEventListener('click', event => {
+		location.href = "reservation.html";
+	})
+})
 
