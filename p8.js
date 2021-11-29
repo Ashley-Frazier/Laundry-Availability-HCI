dcaa4f298d83bd0e72d8f3321d0f6a2d60a0b99d
Ashley-Frazier <afrazier316@gmail.com>
2021-11-29 15:56:32 -0500

    Adding popup and buttons

--- p8.js
+++ p8.js
@@ -1,10 +1,31 @@
 // var dt = new Date();
 // document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
-var reserve = document.querySelectorAll('.reverse-button');
+var reserveAll = document.querySelectorAll('.reverse-button');
+var submit = document.getElementById("submit-button");
+var popup = document.getElementById("confirmation");
+var close = document.getElementById("return-button")
+var dryer = document.getElementById("fl1-dryer-1")
+var dryer_button = document.getElementById("fl1-dryer-1-button")
 
-reserve.forEach(item => {
+
+reserveAll.forEach(item => {
 	item.addEventListener('click', event => {
 		location.href = "reservation.html";
 	})
 })
 
+submit.addEventListener('click', event => {
+	popup.style.display = "block";
+})
+
+close.addEventListener('click', event =>{
+	location.href = "availabilitypage.html";
+	dryer.style.backgroundColor = "gray";
+	dryer_button.innerHTML = "Cancel Reservation"
+})
+
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
+}
