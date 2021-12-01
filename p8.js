8dea23ea3a231f61ed8655cab656ba3219b62a30
Ashley-Frazier <afrazier316@gmail.com>
2021-12-01 15:59:04 -0500

    Update p8.js

--- p8.js
+++ p8.js
@@ -1,29 +1,14 @@
-// var dt = new Date();
-// document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
-var reserveAll = document.querySelectorAll('.reverse-button');
 var submit = document.getElementById("submit-button");
 var popup = document.getElementById("confirmation");
-var close = document.getElementById("return-button")
-var cancel = document.getElementById("cancel-button")
+var close = document.getElementById("return-button");
 
-
-reserveAll.forEach(item => {
-	item.addEventListener('click', event => {
-		location.href = "reservation.html";
-	})
-})
-
-submit.addEventListener('click', event => {
+submit.addEventListener('click', function() {
 	popup.style.display = "block";
-})
+});
 
-close.addEventListener('click', event =>{
+close.addEventListener('click', function(){
 	location.href = "reserved.html";
-})
-
-cancel.addEventListener('click', event =>{
-	location.href = "availabilitypage.html";
-})
+});
 
 window.onclick = function(event) {
   if (event.target == popup) {
