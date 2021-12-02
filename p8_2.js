20f7c8db915fa11821afbafd2bfc30cba557a709
deirdre-mc <70924589+deirdre-mc@users.noreply.github.com>
2021-12-02 13:02:16 -0500

    Update p8_2.js

--- p8_2.js
+++ p8_2.js
@@ -1,12 +1,14 @@
 var cancel = document.getElementById("cancel-button");
 var reserveAll = document.querySelectorAll('.reverse-button');
 
+// Connects reserve button on all machines to reservation page
 reserveAll.forEach(item => {
 	item.addEventListener('click', event => {
 		location.href = "reservation.html";
 	})
 });
 
+// Connects canncel button to original availability page
 cancel.addEventListener('click', function(){
 	location.href = "availabilitypage.html";
 });
