22be8f4a5b53f2161abd365c0fac68679ed693de
deirdre-mc <70924589+deirdre-mc@users.noreply.github.com>
2021-12-02 13:03:46 -0500

    added comments

--- p8.js
+++ p8.js
@@ -2,14 +2,17 @@ var submit = document.getElementById("submit-button");
 var popup = document.getElementById("confirmation");
 var close = document.getElementById("return-button");
 
+// Makes pop up visible when submit button is clicked
 submit.addEventListener('click', function() {
 	popup.style.display = "block";
 });
 
+// Connects ok button to reserved machine page
 close.addEventListener('click', function(){
 	location.href = "reserved.html";
 });
 
+// Makes pop up disappear if screen is clicked 
 window.onclick = function(event) {
   if (event.target == popup) {
     popup.style.display = "none";
